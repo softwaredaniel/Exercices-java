b857a6b2a8a6c578cb2ad72e4571be5106bf372d
Daniel <danielnamster@gmail.com>
2023-02-15 17:50:38 -0500

    add files intelli idea

---
+++ src/com/example/Funciones.java
@@ -0,0 +1,51 @@
+package com.example;
+
+public class Funciones {
+    public static void main(String[] args) {
+        //opcion 1:funcion sin parametros
+    // menu();
+
+        //opcion 2:funcion sin parametros y con retorno
+     String menu =  getMenu();
+     System.out.println(menu);
+
+     //opcion 3:funcion con parametros y sin retorno
+     saludo("Hola");
+     //opcion 4:funcion con parametros y con retorno
+        String nombre="Jose";
+        String  apellido="Solano";
+        String darSaludo=obtenerSaludo(nombre,apellido);
+        System.out.println(darSaludo);
+        /**
+         *  Para este ejercicio tendréis que crear una función que reciba un precio y devuelva el precio con el IVA incluido.
+         */
+        int iva =19;
+        int precio=90000;
+        int calcular=recibirPrecio(iva,precio);
+        System.out.println(calcular);
+    }
+
+
+    /**
+     * void indica que no devuelve nada
+     */
+   static void  menu(){
+        System.out.println("Bienvenido al menu");
+        System.out.println("1 modulo admin");
+    }
+
+    static   String getMenu(){
+       return "Bienvenido al menu \n 1 modulo admin";
+    }
+
+    static  void saludo(String name){
+       System.out.println("Buenas tardes " + name);
+    }
+    static  String obtenerSaludo(String nombre, String apellido){
+        return "Buenas"+nombre + apellido;
+    }
+
+    static  int recibirPrecio(int iva, int precio){
+       return iva+precio;
+    }
+}
---
+++ src/com/example/Operaciones.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class Operaciones {
+    public static void main(String[] args) {
+        int numero1=10;
+        int numero2=5;
+        int numero3=8;
+        boolean resultado=numero1>5&&numero2<numero3;
+        System.out.println(resultado);
+    }
+
+}
