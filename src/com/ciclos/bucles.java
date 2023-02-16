980ce8d98819b2dbc4c53a52054471fe0b117f1b
Daniel <danielnamster@gmail.com>
2023-02-15 19:43:26 -0500

    add files intelli idea

---
+++ src/com/ciclos/bucles.java
@@ -0,0 +1,14 @@
+package com.ciclos;
+
+public class bucles {
+    public static void main(String[] args) {
+        String[] nombres={"Jose","Daniel","ivan","soto"};
+    while(nombres!=null){
+             System.out.println(nombres[0]+nombres[1]);
+             System.out.println(nombres[2]+nombres[3]);
+             break;
+      }
+    }
+
+
+}
